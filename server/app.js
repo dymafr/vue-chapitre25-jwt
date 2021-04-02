5a7b1eff396051354c2d9e8f0bc568ea5cba2b61
dymafr <contact@dyma.fr>
2021-04-02 17:13:48 +0200

    maj

--- server/app.js
+++ server/app.js
@@ -1,18 +1,19 @@
-var express = require("express");
-var path = require("path");
-const mongoose = require("mongoose");
-const index = require("./routes");
+var express = require('express');
+var path = require('path');
+const mongoose = require('mongoose');
+const index = require('./routes');
 
 mongoose.connect(
-  "mongodb+srv://jean:123@cluster0-urpjt.gcp.mongodb.net/chap24vue?retryWrites=true&w=majority",
+  'mongodb+srv://jean:123@cluster0-urpjt.gcp.mongodb.net/chap24vue?retryWrites=true&w=majority',
   {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   },
-  err => {
+  (err) => {
     if (err) {
-      console.log("ERROR DB");
+      console.log('ERROR DB');
     } else {
-      console.log("CONNEXION DB OK !");
+      console.log('CONNEXION DB OK !');
     }
   }
 );
@@ -21,23 +22,23 @@ var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "../client-build")));
+app.use(express.static(path.join(__dirname, '../client-build')));
 
 app.use(index);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client-build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client-build/index.html'));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.json("error");
+  res.json('error');
 });
 
 module.exports = app;
